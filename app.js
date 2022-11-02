beeb5e57ca23d2e2ee5ed52b46dbfc1e7a1e2cce
muhammadahsan2000 <ahsanghanchi06@gmail.com>
2022-11-02 05:13:19 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,47 @@
+alert ( "Thanks for your input!" );
+var a =10;
+var b=20;
+var c=a-b;
+// alert(c);
+console.log(c)
+// var name = "Mark";
+// alert(name);
+
+var nationality;
+nationality = "U.S.";
+alert(nationality);
+var weight = 150;
+console.log(weight);
+
+// var originalNum = "23";
+//  var newNum = originalNum + 7;
+//  console.log(originalNum);
+
+var popularNumber = 2 + 2;
+alert(popularNumber);
+
+var num = 10;
+var popularNumber = num + 200;
+console.log(popularNumber);
+
+var num = 1;
+ var newNum = num++;
+ console.log(newNum);
+
+ var num = 1;
+ var newNum = --num;
+ console.log(newNum);
+
+ var resultOfComputation = (2 * 4) * (4 + 2);
+ console.log(resultOfComputation);
+
+ var message = "Thanks, ";
+ var userName = "Susan";
+ var banger = "!";
+ var customMess = message + userName + banger;
+ console.log(customMess);
+
+ var a=2;
+ var b="2";
+ var c=a+b;
+ console.log(c);
